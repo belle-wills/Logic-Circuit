a12377ee73a06c000ff0c98a01877d103124e128
belle-wills <bellewilliams1@icloud.com>
2025-02-11 17:48:14 +0000

    Update Vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // Ensures relative paths in production builds
-  server: {
-    port: 3000, // Development server port
-    open: true  // Automatically open the app in the browser
-  },
-  build: {
-    outDir: 'dist', // Directory for production build files
-  }
+  plugins: [react()],
+  base: '/Logic-Circuit/', 
 });
